90b5658685a331697e7f3ff4709c5493fd9b7355
Alejandro <alejandrofigueroa3183@gmail.com>
2022-03-29 15:44:07 -0500

    Cleaning

--- WiredNS2.tcl
+++ WiredNS2.tcl
@@ -1,6 +1,4 @@
 #Kaden Carter, Cody Citrano, Alejandro Figueroa - CPSC 4317 Computer Networks - Dr. Bo Sun
 #This Programming Script will demonstrate the simulation of a wired NS2.
 
-
 set ns [new Simulator]
-
