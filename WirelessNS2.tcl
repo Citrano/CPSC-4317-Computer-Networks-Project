8532db8f4f4574a024996b75e64a7b0112db2759
Alejandro <alejandrofigueroa3183@gmail.com>
2022-04-01 12:18:55 -0500

    Proc finish

--- WirelessNS2.tcl
+++ WirelessNS2.tcl
@@ -3,5 +3,16 @@
 
 set ns [new Simulator]
 
+#Finish procedure
+proc finish {} {
+    global ns nf
+    $ns flush-trace
+    #Close the trace
+    close $nf
+    #Execute nam on the trace file
+    exec nam out.nam &
+    exit 0
+}
+
 #Node ***Place the needed number of nodes below. Unsure if still 60 or not.
 set node_number 
