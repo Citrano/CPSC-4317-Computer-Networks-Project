e78dba532feb18dd5c836ce20aefca660107bdb9
Alejandro <alejandrofigueroa3183@gmail.com>
2022-04-01 12:36:10 -0500

    Wireless work

--- WirelessNS2.tcl
+++ WirelessNS2.tcl
@@ -1,7 +1,12 @@
 #Kaden Carter, Cody Citrano, Alejandro Figueroa - CPSC 4317 Computer Networks - Dr. Bo Sun
 #This Programming Script will demonstrate the simulation of a wireless NS2.
 
+#Create simulator object
 set ns [new Simulator]
+
+#Open nam trace file
+set nf [open out.nam w]
+$ns namtrace-all $nf
 
 #Finish procedure
 proc finish {} {
@@ -14,5 +19,14 @@ proc finish {} {
     exit 0
 }
 
-#Node ***Place the needed number of nodes below. Unsure if still 60 or not.
-set node_number 
+#Nodes (wants 60 nodes from parameters)
+set node_number 60
+
+#This is how we connect nodes BETWEEN link and Drop place something link node like $n0 $n1 1Mb 100ms
+$ns duplex-link DropTail
+
+#Call finish procedure - finish at 100 seconds (wants 100 seconds from parameters)
+$ns at 100.0 "finish"
+
+#Runs the simulation
+$ns run
