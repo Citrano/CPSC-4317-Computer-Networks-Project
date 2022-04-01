c53cb2fd34e2a6684e2d595c47d8bcea2aec6646
Alejandro <alejandrofigueroa3183@gmail.com>
2022-04-01 12:37:55 -0500

    comments

--- WirelessNS2.tcl
+++ WirelessNS2.tcl
@@ -25,6 +25,27 @@ set node_number 60
 #This is how we connect nodes BETWEEN link and Drop place something link node like $n0 $n1 1Mb 100ms
 $ns duplex-link DropTail
 
+#UDP agent attachment
+set udp0 [new Agent/UDP]
+$ns attach-agent $n(0) $udp0
+
+#CBR Traffic UDP attachment
+set cbr0 [new Application/Traffic/CBR]
+$cbr0 set packetSize_ 1000
+$cbr0 set interval_ 0.005
+$cbr0 attach-agent $udp0
+
+#Null agent
+set null0 [new Agent/Null]
+$ns attach-agent $n(3) $null0
+
+#Connect source with sink
+$ns connect $udp0 $null0
+
+#Schedule Procedures - finish at 100 seconds (wants 100 seconds from parameters)
+$ns at 0.5 "$cbr0 start"
+$ns at 4.5 "$cbr0 stop"
+
 #Call finish procedure - finish at 100 seconds (wants 100 seconds from parameters)
 $ns at 100.0 "finish"
 
