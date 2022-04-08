ca850b879e75408e697aae0ddf394ea58f43c126
ahplasmind <cartekad000@gmail.com>
2022-04-08 13:38:37 -0500

    edits

--- WirelessTest.tcl
+++ WirelessTest.tcl
@@ -8,34 +8,30 @@ set val(ifq)        Queue/DropTail/PriQueue
 set val(ll)         LL
 set val(ant)        Antenna/OmniAntenna
 set val(ifqlen)     50
-set val(nn)         2
-set val(rp)         AODV
+set val(nn)         60
+set val(rp)         DSDV
+set val(x)          500
+set val(y)          500
+set val(stop)       100
+set val(traffic)    cbr
+set val(traffic)    udp
 
 # main program
 
 set ns [new Simulator]
 
 set nf [open out.nam w]
-$ns namtrace-all $nf
+$ns namtrace-all-wireless $nf $val(x) $val(y)
 
 set tf [open out.tr w]
 $ns trace-all $tf
 
-proc finish {} {
-    global ns nf tf
-    $ns flush-trace
-    close $nf
-    close $tf
-    exec nam out.nam &
-    exit 0
-}
-
 # topography
 set topo [new Topography]
-$topo load_flatgrid 500 500
+$topo load_flatgrid $val(x) $val(y)
 
 # create god
-create-god $val(nn)
+set god_ [create-god $val(nn)]
 
 # config nodes
 $ns node-config -adhocRouting $val(rp) \
@@ -51,45 +47,40 @@ $ns node-config -adhocRouting $val(rp) \
                 -agentTrace ON \
                 -routerTrace ON \
                 -macTrace ON \
-                -movementTrace OFF
+                -movementTrace ON
 
 for {set i 0} {$i < $val(nn)} {incr i} {
     set n($i) [$ns node]
-    $n($i) random-motion 0
 }
 
-$n(0) set X_ 5.0
-$n(0) set Y_ 2.0
-$n(0) set Z_ 0.0
+for {set i 0} {$i < val(nn)} {incr i} {
+    $ns initial_node_pos $n($i) 30
+}
 
-$n(1) set X_ 100.0
-$n(1) set Y_ 100.0
-$n(1) set Z_ 0.0
+proc destination {} {
+    global ns val n
+    set time 1.0
+    set now [$ns now]
+    for {set i 0} {$i < $val(nn)} {incr i} {
+        set xx [expr rand()*400]
+        set yy [expr rand()*250]
+        $ns at $now "$n($i) setdest $xx $yy 0.0
+    }
 
-# node movements
+    $ns at [expr $now+$time] "destination"
+}
 
-$ns at 5.0 "$n(1) setdest 25.0 20.0 0.0"
-$ns at 7.0 "$n(0) setdest 20.0 25.0 0.0"
+$ns at $val(stop) "$ns nam-end-wireless $val(stop)"
+$ns at $val(stop) "stop"
 
-$ns at 15.0 "$n(1) setdest 50.0 50.0 0.0"
+$ns at 10.5 "puts \"end simulation\"; $ns halt"
 
-# traffic flow
-set udp0 [new Agent/UDP]
-$ns attach-agent $n(0) $udp0
-
-set cbr0 [new Application/Traffic/CBR]
-$cbr0 set packetSize_ 1000
-$cbr0 set interval_ 0.005
-$cbr0 attach-agent $udp0
-
-set null0 [new Agent/Null]
-$ns attach-agent $n(1) $null0
-
-$ns connect $udp0 $null0
-
-$ns at 0.5 "$cbr0 start"
-$ns at 4.5 "$cbr0 stop"
-
-$ns at 100.0 "finish"
+proc stop {} {
+    global ns nf tf
+    $ns flush-trace
+    close $nf
+    close $tf
+    exec nam out.nam &
+}
 
 $ns run
