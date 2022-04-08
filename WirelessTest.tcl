ba528bd943650fd4d680f4c072245a6d8a9436d4
ahplasmind <cartekad000@gmail.com>
2022-04-08 13:06:01 -0500

    new wireless testing file

---
+++ WirelessTest.tcl
@@ -0,0 +1,86 @@
+# define options
+
+set val(chan)       Channel/WirelessChannel
+set val(prop)       Propagation/TwoRayGround
+set val(netif)      Phy/WirelessPhy
+set val(mac)        Mac/802_11
+set val(ifq)        Queue/DropTail/PriQueue
+set val(ll)         LL
+set val(ant)        Antenna/OmniAntenna
+set val(ifqlen)     50
+set val(nn)         2
+set val(rp)         AODV
+
+# main program
+
+set ns [new Simulator]
+
+set nf [open out.nam w]
+$ns namtrace-all $nf
+
+set tf [open out.tr w]
+$ns trace-all $tf
+
+# topography
+set topo [new Topography]
+$topo load_flatgrid 500 500
+
+# create god
+create-god $val(nn)
+
+# config nodes
+$ns node-config -adhocRouting $val(rp) \
+                -llType $val(ll) \
+                -macType $val(mac) \
+                -ifqType $val(ifq) \
+                -ifqLen $val(ifqlen) \
+                -antType $val(ant) \
+                -propType $val(prop) \
+                -phyType $val(netif) \
+                -channelType $val(chan) \
+                -topoInstance $topo \
+                -agentTrace ON \
+                -routerTrace ON \
+                -macTrace ON \
+                -movementTrace OFF
+
+for {set i 0} {$i < $val(nn)} {incr i} {
+    set n($i) [$ns node]
+    $n($i) random-motion 0
+}
+
+$n(0) set X_ 5.0
+$n(0) set Y_ 2.0
+$n(0) set Z_ 0.0
+
+$n(1) set X_ 100.0
+$n(1) set Y_ 100.0
+$n(1) set Z_ 0.0
+
+# node movements
+
+$ns at 5.0 "$n(1) setdest 25.0 20.0 0.0"
+$ns at 7.0 "$n(0) setdest 20.0 25.0 0.0"
+
+$ns at 15.0 "$n(1) setdest 50.0 50.0 0.0"
+
+# traffic flow
+set udp0 [new Agent/UDP]
+$ns attach-agent $n(0) $udp0
+
+set cbr0 [new Application/Traffic/CBR]
+$cbr0 set packetSize_ 1000
+$cbr0 set interval_ 0.005
+$cbr0 attach-agent $udp0
+
+set null0 [new Agent/Null]
+$ns attach-agent $n(1) $null0
+
+$ns connect $udp0 $null0
+
+$ns at 0.5 "$cbr0 start"
+$ns at 4.5 "$cbr0 stop"
+
+$ns at 100.0 "finish"
+
+$ns run
